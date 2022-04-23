42ea96b7cb457dd742af8bcd5c5b23e9d53df04d
iliaivashkin <famousmrbacon@gmail.com>
2022-04-23 22:12:02 +0800

    js start

---
+++ js_lessons/booleans.js
@@ -0,0 +1,4 @@
+'Если нужно получить ответ да или нет используются булевые значения'
+var kitchenLights = false;
+kitchenLights = true;
+kitchenLights;
---
+++ js_lessons/strings.js
@@ -0,0 +1,5 @@
+'Привет мир!'
+"Нужно помнить о вежливости и говорить 'спасибо' и 'пожалуйста'"
+
+"earthshake".length
+console.log("earthshake".length)
